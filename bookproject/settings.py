428b1b1bdcb7e054b927ee84cfb97510c87a2d1f
okunokosei <okuno.kosei1014@gmail.com>
2025-10-21 13:07:23 +0900

    Add STATIC_ROOT setting for collectstatic

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
